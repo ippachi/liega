325cf6b2cfc3c1edf02f9ae66c08270308f8672d
ippachi <ippachi1018@gmail.com>
2023-01-29 18:59:13 +0900

    Reset all

--- lib/liega/persistence.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Liega
-  module Persistence
-    DB = Sequel.connect(ENV.fetch("DATABASE_URL"))
-  end
-end
--- lib/liega/persistence/user_repository.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Liega
-  module Persistence
-    class UserRepository < Repository
-      relation_name :users
-    end
-  end
-end
