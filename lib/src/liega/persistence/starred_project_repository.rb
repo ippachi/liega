d190cd79c4a0d6b7b44741e87190999196c29ca1
ippachi <ippachi1018@gmail.com>
2023-03-11 00:34:26 +0900

    fix: Dupcalite star

--- lib/src/liega/persistence/starred_project_repository.rb
+++ lib/src/liega/persistence/starred_project_repository.rb
@@ -9,14 +9,27 @@ module Liega
         starred_project = StarredProject.find_by!(code:)
         user_code = starred_project.user.code
         project_code = starred_project.project.code
-        attributes = aggregate_root_attributes(starred_project).except(:user_id, :project_id)
-        Domain::Model::StarredProject.new(**attributes, user_code:, project_code:)
+        create_instance(starred_project, user_code, project_code)
+      end
+
+      def find_by_user_and_project_or_nil(user_code, project_code)
+        user = User.find_by(code: user_code)
+        project = Project.find_by(code: project_code)
+        starred_project = StarredProject.find_by(user:, project:)
+        starred_project ? create_instance(starred_project, user_code, project_code) : nil
       end
 
       def save(starred_project, lock_version = nil)
         user_id = User.find_by!(code: starred_project.user_code).id
         project_id = Project.find_by!(code: starred_project.project_code).id
         save_relation(starred_project, { code: starred_project.code, user_id:, project_id: }, lock_version)
+      end
+
+      private
+
+      def create_instance(starred_project, user_code, project_code)
+        attributes = aggregate_root_attributes(starred_project).except(:user_id, :project_id)
+        Domain::Model::StarredProject.new(**attributes, user_code:, project_code:)
       end
     end
   end
--- test/liega/app/star_project_test.rb
+++ test/liega/app/star_project_test.rb
@@ -11,6 +11,15 @@ module Liega
         starred_project = StarProject.new.call(user_code, project_code)
         assert starred_project_repo.find(starred_project.code)
       end
+
+      test "star same project" do
+        user_code = users(:normal).code
+        project_code = projects(:default).code
+        assert_nothing_raised do
+          StarProject.new.call(user_code, project_code)
+          StarProject.new.call(user_code, project_code)
+        end
+      end
     end
   end
 end
